ab3960e291bddec9594ee050ad0f3387e95a623b
William Huang <whuang6@stanford.edu>
2015-06-18 20:14:51 -0700

    loginviewcontroller created

---
+++ ActivityFeed/ActivityFeed/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ActivityFeed
+//
+//  Created by William Huang on 6/18/15.
+//  Copyright (c) 2015 William Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
