54aaf899fcdb564d13221b2dd116e5f8e287f20f
William Huang <whuang6@stanford.edu>
2015-06-18 19:17:45 -0700

    Initial project create

---
+++ ActivityFeed/ActivityFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ActivityFeed
+//
+//  Created by William Huang on 6/18/15.
+//  Copyright (c) 2015 William Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ActivityFeed/ActivityFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActivityFeed
+//
+//  Created by William Huang on 6/18/15.
+//  Copyright (c) 2015 William Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
